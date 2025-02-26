ce20749ce2749e37a43e7e64a15b727017fb1146
denigori <44337205+denigori@users.noreply.github.com>
2025-02-26 23:34:17 +0100

    commented out a line

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config(); // Import dotenv
 const express = require('express'); // Import express
 const cors = require('cors'); // Import cors
-
+const db = require('./db'); // Import the db module
 
 const app = express();       // Create an express app
 const port = 3000;           // Set the port
 
 app.use(cors());           // Use the cors middleware
-app.use(bodyParser.json()); // Use the body-parser middleware
+//app.use(bodyParser.json()); // Use the body-parser middleware
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
